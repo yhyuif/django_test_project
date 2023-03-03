2e9334ec43c06b089d7f762fb8fc8e95d7a9e57e
yangyuchen <rrryangyuchen@qq.com>
2023-03-03 14:58:56 +0800

    add test

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import datetime
 
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() returns False for questions whose pub_date
+		is in the future.
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		message = 'something is wrong!'
+		self.assertIs(future_question.was_published_recently(), False, message)
+
+
+
+
+
